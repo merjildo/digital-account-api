d45ba2ebb0ffda5218941782f7694ede69e1570a
diego fernandez <diegoaqp@gmail.com>
2022-09-21 20:11:12 -0300

    Migrate to swagger3 in order to enable actuator.

--- src/main/java/com/nasc/digitalAccount/config/SwaggerConfig.java
+++ src/main/java/com/nasc/digitalAccount/config/SwaggerConfig.java
@@ -1,30 +1,41 @@
 package com.nasc.digitalAccount.config;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 @Configuration
-@EnableSwagger2
+//@EnableSwagger2
 public class SwaggerConfig {
-    public static final Contact DEFAULT_CONTACT = new Contact("Diego Merjildo", "https://github.com/merjildo", "diego.merjildo@gmail.com");
-    public static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Digital Account API",
-            "Digital Account API",
-            "1.0", "urn:tos",
-            DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0", Arrays.asList());
+//    public static final Contact DEFAULT_CONTACT = new Contact("Diego Merjildo", "https://github.com/merjildo", "diego.merjildo@gmail.com");
+//    public static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Digital Account API",
+//            "Digital Account API",
+//            "1.0", "urn:tos",
+//            DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0", Arrays.asList());
     private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<>(Arrays.asList("application/json", "application/xml"));
 
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).apiInfo(DEFAULT_API_INFO)
-                .produces(DEFAULT_PRODUCES_AND_CONSUMES).consumes(DEFAULT_PRODUCES_AND_CONSUMES);
-    }
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.SWAGGER_2).apiInfo(DEFAULT_API_INFO)
+//                .produces(DEFAULT_PRODUCES_AND_CONSUMES).consumes(DEFAULT_PRODUCES_AND_CONSUMES);
+//    }
+@Bean
+public OpenAPI springShopOpenAPI() {
+    return new OpenAPI()
+            .info(new Info().title("Digital Account API")
+                    .description("Digital Account API")
+                    .version("v0.0.1")
+                    .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+            .externalDocs(new ExternalDocumentation()
+                    .description("Diego F. Merjildo")
+                    .url("https://github.com/merjildo"));
+}
 }
--- src/main/java/com/nasc/digitalAccount/controller/ClientController.java
+++ src/main/java/com/nasc/digitalAccount/controller/ClientController.java
@@ -2,9 +2,9 @@ package com.nasc.digitalAccount.controller;
 
 import com.nasc.digitalAccount.dto.ClientDto;
 import com.nasc.digitalAccount.service.ClientService;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -21,10 +21,10 @@ public class ClientController {
 
     @PostMapping("/clients")
     @ResponseStatus(HttpStatus.OK)
-    @ApiOperation(value = "Registration new client account", notes = "Allows new client registration")
+    @Operation(summary = "Registration new client account", description = "Allows new client registration")
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Successfully created"),
-            @ApiResponse(code = 409, message = "Conflict - The resource already exists")
+            @ApiResponse(responseCode = "200", description = "Successfully created"),
+            @ApiResponse(responseCode = "409", description = "Conflict - The resource already exists")
     })
     public ClientDto createClient(@Valid @RequestBody ClientDto clientDto) {
         return clientService.createClient(clientDto);
@@ -32,42 +32,42 @@ public class ClientController {
 
     @PutMapping("/clients/{id}")
     @ResponseStatus(HttpStatus.OK)
-    @ApiOperation(value = "Update client account", notes = "Allows new client updating")
+    @Operation(summary = "Update client account", description = "Allows new client updating")
     public ClientDto updateClient(@PathVariable(value = "id") Long id, @Valid @RequestBody ClientDto clientDto) {
         return clientService.updateClientInfo(id, clientDto);
     }
 
     @DeleteMapping("/clients/{email}")
     @ResponseStatus(HttpStatus.OK)
-    @ApiOperation(value = "Remove client account", notes = "Allows to client removing")
+    @Operation(summary = "Remove client account", description = "Allows to client removing")
     public void removeClient(@PathVariable(value = "email") String email) {
         clientService.deleteClient(email);
     }
 
     @GetMapping("/clients/findByEmail/{email}")
     @ResponseStatus(HttpStatus.OK)
-    @ApiOperation(value = "Find client by email", notes = "Get client by email")
+    @Operation(summary = "Find client by email", description = "Get client by email")
     public ClientDto findByEmail(@PathVariable(value = "email") String email) {
         return clientService.findByEmail(email);
     }
 
     @GetMapping("/clients")
     @ResponseStatus(HttpStatus.OK)
-    @ApiOperation(value = "Find all clients", notes = "Get all clients")
+    @Operation(summary = "Find all clients", description = "Get all clients")
     public List<ClientDto> findAll() {
         return clientService.findAll();
     }
 
     @GetMapping("/clients/findByName/{name}")
     @ResponseStatus(HttpStatus.OK)
-    @ApiOperation(value = "Find client by name", notes = "Get client by name")
+    @Operation(summary = "Find client by name", description = "Get client by name")
     public ClientDto findByName(@PathVariable(value = "name") String name) {
         return clientService.findByName(name);
     }
 
     @GetMapping("/clients/findByDocument/{document}")
     @ResponseStatus(HttpStatus.OK)
-    @ApiOperation(value = "Find client by CPF/CNPJ", notes = "Get client by CPF/CNPJ")
+    @Operation(summary = "Find client by CPF/CNPJ", description = "Get client by CPF/CNPJ")
     public ClientDto findByDocument(@PathVariable(value = "document") String document) {
         return clientService.findByDocument(document);
     }
